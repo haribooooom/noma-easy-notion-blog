982b7b3f119a9dcf83d5b278c85800e4049269ec
haribooooom <haribooooom@gmail.com>
2022-12-28 13:09:42 +0900

    change home txt

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,9 @@ const RootPage = () => (
     <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
     <div className={styles.container}>
       <div>
-        <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
-        <p>
-          easy-notion-blog powered by{' '}
-          <Link href="https://github.com/otoyo/easy-notion-blog">
-            otoyo/easy-notion-blog
-          </Link>
-        </p>
+        <h2>こんにちは！</h2>
+        <p>このページはNotionだったらブログが続くのではと目論むのまのNotionブログです</p>
+        <p>日々の学びや感じたこと、良かったことをつらつら書いていきます</p>
       </div>
     </div>
   </>
